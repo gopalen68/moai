5b1cf8fe5f3c04c61d35f18b8d9dd57bb031d05b
Gopalen Parasuraman <pmgopalen@gmail.com>
2025-08-27 22:13:26 +0400

    feat: added logo stack

--- src/components/LogoCloud.tsx
+++ src/components/LogoCloud.tsx
@@ -1,4 +1,9 @@
 import { useState } from 'react'
+import wooLogo from '@/assets/woo_logo_color.png'
+import odooLogo from '@/assets/odoo_logo_white.png'
+import adobeLogo from '@/assets/adobe_logo_red.png'
+import pythonLogo from '@/assets/python_logo_color.png'
+import workflowLogo from '@/assets/workflow_logo_color.png'
 
 export default function LogoCloud() {
     const [isOpen, setIsOpen] = useState(false)
@@ -24,43 +29,43 @@ export default function LogoCloud() {
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
                         <img
-                            alt="Transistor"
-                            src="https://tailwindcss.com/plus-assets/img/logos/158x48/transistor-logo-white.svg"
+                            alt="Odoo"
+                            src={odooLogo}
                             width={158}
                             height={48}
-                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            className="col-span-2 max-h-16 w-full object-contain lg:col-span-1"
                         />
 
                         <img
-                            alt="Reform"
-                            src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-white.svg"
+                            alt="Adobe Commerce"
+                            src={adobeLogo}
                             width={158}
                             height={48}
-                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            className="col-span-2 max-h-16 w-full object-contain lg:col-span-1"
                         />
 
                         <img
-                            alt="Tuple"
-                            src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-white.svg"
+                            alt="WooCommerce"
+                            src={wooLogo}
                             width={158}
                             height={48}
-                            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                            className="col-span-2 max-h-16 w-full object-contain lg:col-span-1"
                         />
 
                         <img
-                            alt="SavvyCal"
-                            src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-white.svg"
+                            alt="Workflow Automation"
+                            src={workflowLogo}
                             width={158}
                             height={48}
-                            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                            className="col-span-2 max-h-16 w-full object-contain sm:col-start-2 lg:col-span-1"
                         />
 
                         <img
-                            alt="Statamic"
-                            src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-white.svg"
+                            alt="Python"
+                            src={pythonLogo}
                             width={158}
                             height={48}
-                            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            className="col-span-2 col-start-2 max-h-16 w-full object-contain sm:col-start-auto lg:col-span-1"
                         />
                     </div>
                 </div>
